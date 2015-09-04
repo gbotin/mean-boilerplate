fdb3e74d5b3954ca0bdf68bb1bc59b3423cfe416
gbotin <geoffrey.botin@gmail.com>
2015-09-04 10:53:30 +0200

    Update gulpfile with livereload

--- gulpfile.es6.js
+++ gulpfile.es6.js
@@ -5,31 +5,44 @@ import nodemon from 'gulp-nodemon';
 import livereload from 'gulp-livereload';
 import sass from 'gulp-sass';
 
-gulp.task("build", () => {
+gulp.task("server", () => {
   gulp.src("./server/bin/www")
     .pipe(gulp.dest("dist/server/bin"));
 
   gulp.src("./server/**/*.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist/server"));
+    .pipe(gulp.dest("./dist/server"));
+});
+
+gulp.task("scripts", () => {
+  gulp.src("./client/src/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("./dist/client/js"))
+});
+
+gulp.task("sass", () => {
+  gulp.src("./client/style/**/*.{sass,scss}")
+    .pipe(sass())
+    .pipe(gulp.dest("./dist/client/css"))
 });
 
 gulp.task("html", () => {
   gulp.src("./client/**/*.html")
-    .pipe(gulp.dest('./dist/client'));
+    .pipe(gulp.dest('./dist/client'))
 });
 
-gulp.task("sass", () => {
-  gulp.src("./client/style/**/*.sass")
-    .pipe(sass())
-    .pipe(gulp.dest("dist/client/css"));
-});
+gulp.task("build", ["server", "scripts", "html", "sass"]);
 
 gulp.task("watch", () => {
   livereload.listen();
 
-  // gulp.watch("./dist/server/**/*.js", ["build"]);
+  gulp.watch("./client/src/**/*.js", ["scripts"]);
+  gulp.watch("./client/style/**/*.sass", ["sass"]);
+  gulp.watch("./client/**/*.html", ["html"]);
 
+  gulp.watch("./dist/client/**/*", (file) => {
+    livereload.changed(file.path);
+  });
 });
 
 gulp.task("serve", () => {
@@ -37,11 +50,11 @@ gulp.task("serve", () => {
     script: './dist/server/bin/www',
     watch: './server',
     ext: 'js',
-    tasks: ["build"],
+    tasks: ["server"],
     env: { 'NODE_ENV': 'development' }
   }).on('restart', () => {
     setTimeout(() => { livereload.reload() }, 1000);
   });
 });
 
-gulp.task("default", ["build", "sass", "html", "serve", "watch"]);
+gulp.task("default", ["build", "serve", "watch"]);
